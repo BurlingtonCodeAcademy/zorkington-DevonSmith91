02b58d42a5a60e7d28f84ec84799e7f9fa01572e
DevonSmith91 <DevonSmith91@gmail.com>
2020-02-13 08:52:56 -0500

    Initial commit, First story finished

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ let states = {
   'roomThree': { canChangeTo: [ 'roomOne' ] }
 };
 
-let currentState = "green";
+let currentState = "roomOne";
 
 function enterState(newState) {
   let validTransitions = states[currentState].canChangeTo;
@@ -29,11 +29,23 @@ function enterState(newState) {
 start();
 
 async function start() {
-  const welcomeMessage = `182 Main St.
-You are standing on Main Street between Church and South Winooski.
-There is a door here. A keypad sits on the handle.
-On the door is a handwritten sign.`;
+  const welcomeMessage = `You heard about a haunted house in the next town over. Curiosity got the best of 
+  you so you decided to go check it out on your day off. As you arrive you see a rundown, two story house. 
+  You notice the windows are covered in filth and no longer allow the light through. The overgrowth of plant 
+  life indicates that nobody has lived here for years. You approach a creepy red door that’s stained with 
+  dirt and mold. As you reach for the handle you notice that it appears to be heavily used, even to this day. 
+  You think nothing of it. As you walk through the door, a chill runs down your spine. Before you realize it 
+  the door closes behind you with an audible *click*. You start to panic. 
+
+  In front of you appears the ghost of a little girl. Her skin is pale and her dress that was once blue is now 
+  stained and tattered. Her jet black hair covers her face. As she raises her head slowly you notice her 
+  lifeless eyes looking at you and you hear her say “I love making new friends. Did you come here to play 
+  with me? If not you better find a way out fast!” She slowly fades out of sight.
+  
+  You’re in the front entrance. 
+  There’s the door you came in behind you and a door into what looks like a living room in front of you. 
+  What would you like to do?\n>_`;
   let answer = await ask(welcomeMessage);
-  console.log('Now write your code to make this work!');
+  console.log("I'm sorry, I dont know how to " + answer);
   process.exit();
 }
